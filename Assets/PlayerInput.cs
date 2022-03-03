60cd46a26fdfd56c8e85ff9768c726a1f3ce5997
JustinvdLans <26590@ma-web.nl>
2022-03-03 16:01:18 +0100

    First start on the zoom

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -6,6 +6,9 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField]
     float Sensetivity = 1;
+
+    float zoomLevel; //Only for testing purposes
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,31 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(zoomLevel);
         transform.Rotate(0f, Input.GetAxis("Mouse X") * Sensetivity, 0f, Space.Self);
+
+
+        if (Input.GetMouseButton(1))
+	    {
+            //Play animation cam going to screen
+
+
+            TakePicture(); //Zoom in and out
+
+            ZoomInAndOut(); //Call Take a picture on left mouse button
+	    }
+
+
     }
+
+    void TakePicture()
+    {
+
+    }
+
+    void ZoomInAndOut()
+    {
+
+    }
+
 }
