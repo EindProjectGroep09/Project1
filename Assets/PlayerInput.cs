03f09ba7f19eee7203d4117a76a95105c8bd8d5f
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2022-03-04 15:41:27 +0100

    henk

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -12,6 +12,26 @@ public class PlayerInput : MonoBehaviour
         
     }
 
+    void FixedUpdate(){
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = LayerMask.GetMask("items");
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
