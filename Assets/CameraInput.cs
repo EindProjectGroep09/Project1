50da74272fe9fa104212634fbe37df017f755b27
JustinvdLans <26590@ma-web.nl>
2022-03-09 13:03:51 +0100

    Testcomment

--- Assets/CameraInput.cs
+++ Assets/CameraInput.cs
@@ -66,7 +66,7 @@ public class CameraInput : MonoBehaviour
 
     void OnGUI()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;//
     }
 
 
